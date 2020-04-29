225e786cd9fb3ddfa61af9da132b1ee9695f1f7a
Macbook <macbook@Macbooks-MacBook-Pro.local>
2020-04-29 00:29:47 -0500

    Primeng table and button

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
+import {ButtonModule} from 'primeng/button';
+import {TableModule} from 'primeng/table';
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ListClaimComponent } from './list-claim/list-claim.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListClaimComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    TableModule,
+    ButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/list-claim/list-claim.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-list-claim',
+  templateUrl: './list-claim.component.html',
+  styleUrls: ['./list-claim.component.css']
+})
+export class ListClaimComponent implements OnInit {
+  cars: any[];
+  constructor() {
+    this.cars=[];
+    this.cars.push({vin: 'stm933', color: 'black', year: 2011, brand: 'Renault'});
+  }
+
+  ngOnInit(): void {
+  }
+
+}
